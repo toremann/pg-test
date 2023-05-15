f59da8bd8b96fce5ebc9332c7d35481522e20510
toremann <50668920+toremann@users.noreply.github.com>
2023-05-15 21:45:14 +0200

    sql queries

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 ## Insert data to postgress
+
+To get all jobs for a specific location: GET /jobs?lokasjon=OSLO
+To get the latest job for a specific location: GET /jobs?lokasjon=OSLO&latest=true
--- app.js
+++ app.js
@@ -14,21 +14,29 @@ const pool = new Pool({
 });
 
 app.get("/jobs", async (req, res) => {
-  const { lokasjon } = req.query;
-  if (!lokasjon) {
-    return res.status(400).json({ error: "Missing lokasjon parameter" });
+  const { lokasjon, order } = req.query;
+  let query = "SELECT * FROM jobs";
+  let params = [];
+
+  if (lokasjon) {
+    query += " WHERE lokasjon = $1";
+    params.push(lokasjon);
   }
+
+  if (order === "last") {
+    query += " ORDER BY to_date(dato, 'DD/MM/YYYY') DESC LIMIT 1";
+  } 
+
   try {
-    const { rows } = await pool.query(
-      "SELECT * FROM jobs WHERE lokasjon = $1",
-      [lokasjon]
-    );
+    const { rows } = await pool.query(query, params);
     res.json(rows);
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: "Internal server error" });
   }
 });
+
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
