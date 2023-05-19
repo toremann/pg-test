6e2bda72ae73da00fb27b7cb8c92dc1d1f0d86c5
toremann <50668920+toremann@users.noreply.github.com>
2023-05-19 20:58:55 +0200

    deploy

--- app.js
+++ app.js
@@ -18,4 +18,4 @@ app.use("/", viewRoutes);
 app.use("/", sortRoutes);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
